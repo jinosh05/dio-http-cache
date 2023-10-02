95f212755b7a6a64655390fa9f2253afac78534d
Jinosh05 <jinoshprabhuraj5@gmail.com>
2023-10-02 23:58:17 +0530

    updated  dependencies

--- example/lib/panels/cache_manage.dart
+++ example/lib/panels/cache_manage.dart
@@ -57,16 +57,12 @@ class _CacheManagerPanelState extends State<CacheManagerPanel> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Text("Cache Manager",
-                  style: Theme.of(context)
-                      .textTheme
-                      .headline6!
-                      .copyWith(color: Theme.of(context).accentColor)),
+                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.secondary)),
               Container(height: 50),
               Text("1. Choose mode:",
-                  style: Theme.of(context)
-                      .textTheme
-                      .subtitle2!
-                      .copyWith(color: Theme.of(context).accentColor)),
+                  style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                      color: Theme.of(context).colorScheme.secondary)),
               DropdownButton<_Mode>(
                   value: _mode,
                   onChanged: (value) => setState(() => _mode = value),
@@ -87,17 +83,15 @@ class _CacheManagerPanelState extends State<CacheManagerPanel> {
               for (var w in getSubKeyViews(context)) w,
               Container(height: 20),
               Text("${getLabel()}. to clear",
-                  style: Theme.of(context)
-                      .textTheme
-                      .subtitle2!
-                      .copyWith(color: Theme.of(context).accentColor)),
+                  style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                      color: Theme.of(context).colorScheme.secondary)),
               Padding(
                   padding: EdgeInsets.all(10),
                   child: FloatingActionButton(
                       child: Text("Clear",
                           style: Theme.of(context)
                               .textTheme
-                              .subtitle2!
+                              .titleSmall!
                               .copyWith(color: Colors.white)),
                       onPressed: () => _clear()))
             ]));
@@ -132,11 +126,11 @@ class _CacheManagerPanelState extends State<CacheManagerPanel> {
       Text("2. RequestMethod:",
           style: Theme.of(context)
               .textTheme
-              .subtitle2!
-              .copyWith(color: Theme.of(context).accentColor)),
+              .titleSmall!
+              .copyWith(color: Theme.of(context).colorScheme.secondary)),
       TextField(
           controller: _requestMethodController,
-          style: Theme.of(context).textTheme.bodyText1),
+          style: Theme.of(context).textTheme.bodyLarge),
       Container(height: 20),
     ];
   }
@@ -148,11 +142,11 @@ class _CacheManagerPanelState extends State<CacheManagerPanel> {
       Text("3. Key:",
           style: Theme.of(context)
               .textTheme
-              .subtitle2!
-              .copyWith(color: Theme.of(context).accentColor)),
+              .titleSmall!
+              .copyWith(color: Theme.of(context).colorScheme.secondary)),
       TextField(
           controller: _keyController,
-          style: Theme.of(context).textTheme.bodyText1),
+          style: Theme.of(context).textTheme.bodyLarge),
       Container(height: 20),
     ];
   }
@@ -164,11 +158,11 @@ class _CacheManagerPanelState extends State<CacheManagerPanel> {
       Text("4. Subkey:",
           style: Theme.of(context)
               .textTheme
-              .subtitle2!
-              .copyWith(color: Theme.of(context).accentColor)),
+              .titleSmall!
+              .copyWith(color: Theme.of(context).colorScheme.secondary)),
       TextField(
           controller: _subKeyController,
-          style: Theme.of(context).textTheme.bodyText1),
+          style: Theme.of(context).textTheme.bodyLarge),
       Container(height: 20),
     ];
   }
